8e161cc1493744e86c9e786e2712ae4c98675495
Huguito <>
2024-09-23 00:15:35 -0300

    metodos de producto

--- Back/Producto.py
+++ Back/Producto.py
@@ -37,3 +37,46 @@ class Producto(BaseModel):
         return self.modelo
     def setmodelo(self, new):
         self.modelo = new
+
+    def agregar_categoria(self, categoria):
+        ProductoCategoria.create(producto=self, categoria=categoria)
+
+    def eliminar_categoria(self, categoria):
+        try:
+            relacion = ProductoCategoria.get(ProductoCategoria.producto == self, ProductoCategoria.categoria == categoria)
+            relacion.delete_instance()
+        except ProductoCategoria.DoesNotExist:
+            raise ValueError("La categoría no está asociada a este producto.")
+        
+    def get_id_categorias(self):
+        categorias = (ProductoCategoria.select(ProductoCategoria.categoria).where(ProductoCategoria.producto == self))
+        return [cat.categoria.id for cat in categorias]
+    
+    def get_nombre_categorias(self):
+        categorias = (ProductoCategoria.select(ProductoCategoria.categoria).where(ProductoCategoria.producto == self))
+        return [cat.categoria.nombre for cat in categorias]
+    
+    def lista_productos_por_categoria(self,cat):
+        productos = (ProductoCategoria.select(ProductoCategoria.producto).where(ProductoCategoria.categoria == cat))
+        return [producto.producto for producto in productos]
+    
+    def obtener_datos_producto(self):
+        return [self.iden, self.descripcion, self.precio, self.stock, self.marca, self.modelo]
+    
+    def añadir_producto(self,descripcion = None, precio = None, stock = None, marca = None, modelo = None):
+        Producto.create(descripcion=descripcion, precio=precio, stock=stock, marca=marca, modelo=modelo)
+
+    def eliminar_producto(self):
+        self.delete_instance()
+
+    def modificar_producto(self, descripcion = None, precio = None, stock = None, marca = None, modelo = None):
+        if (descripcion != None) and (descripcion != self.descripcion):
+            self.descripcion = descripcion
+        if (precio != None) and (precio != self.precio):
+            self.precio = precio
+        if (stock != None) and (stock != self.stock):
+            self.stock = stock
+        if (marca != None) and (marca != self.marca):
+            self.marca = marca
+        if (modelo != None) and (modelo != self.modelo):
+            self.modelo = modelo
