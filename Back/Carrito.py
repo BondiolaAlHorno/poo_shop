c2994f0713566d4255b8795e9337038259e60cd1
Octa <th0ryod1n45@gmail.com>
2024-09-29 14:20:58 -0300

    Metodos de Carrito y Categoria

--- Back/Carrito.py
+++ Back/Carrito.py
@@ -19,3 +19,41 @@ class Carrito(BaseModel):
         return self.cliente
     def setcliente(self,new):
         self.cliente = new
+    
+    @classmethod
+    def crear_carrito(cls, cliente):
+        return cls.create(total=0, cliente=cliente)
+
+    def anadir_producto_al_carrito(self, producto, cantidad):
+        CarritoProducto.create(carrito=self, producto=producto, cantidad=cantidad)
+        self.calcular_total()
+
+    def modificar_carrito(self, producto, cantidad):
+        carrito_producto = CarritoProducto.get_or_none(
+            (CarritoProducto.carrito == self) & (CarritoProducto.producto == producto)
+        )
+        if carrito_producto:
+            if cantidad > 0:
+                carrito_producto.cantidad = cantidad
+                carrito_producto.save()
+            else:
+                carrito_producto.delete_instance()
+            self.calcular_total()
+            return True
+        return False
+
+    def confirmar_carrito(self):
+        # Aca tendría que ir la lógica para confirmar el carrito
+        pass
+
+    def mostrar_carrito(self):
+        return list(CarritoProducto.select().where(CarritoProducto.carrito == self))
+
+    def calcular_total(self):
+        total = sum(cp.producto.precio * cp.cantidad for cp in self.mostrar_carrito())
+        self.total = total
+        self.save()
+
+    @classmethod
+    def obtener_carrito_por_cliente(cls, cliente):
+        return cls.get_or_none(cls.cliente == cliente)
--- Back/Categoria.py
+++ Back/Categoria.py
@@ -13,3 +13,30 @@ class Categoria(BaseModel):
         return self.nombre
     def setnombre(self, new):
         self.nombre = new
+        
+    @classmethod
+    def agregar_categoria(cls, nombre):
+        return cls.create(nombre=nombre)
+
+    @classmethod
+    def eliminar_categoria(cls, iden):
+        categoria = cls.get_or_none(cls.iden == iden)
+        if categoria:
+            categoria.delete_instance()
+            return True
+        return False
+
+    @classmethod
+    def modificar_categoria(cls, iden, nuevo_nombre):
+        categoria = cls.get_or_none(cls.iden == iden)
+        if categoria:
+            categoria.nombre = nuevo_nombre
+            categoria.save()
+            return True
+        return False
+
+    @classmethod
+    def obtener_todas_las_categorias(cls):
+        return list(cls.select())
+        
+    # No se si esto esta bien, huguito confirmalo
