31bf7829938e57dcc09d8b2decd204b9f425c615
Zoka <ezequielguzman.ake@gmail.com>
2024-10-12 21:55:49 -0300

    se termino la clase administrador, y  se adelanto la clase cliente

--- Back/Administrador.py
+++ Back/Administrador.py
@@ -5,9 +5,8 @@ class Administrador(Persona): #definimos la clase admnin
     pass #segui scroleando aca no hay nada
 
 #pasamos a definir los metodos
-    def altaAdmin(self, id,nombre,apellido,telefono): # como fubnciona
-        Administrador.create(persona)
-
-        
-    def eliminarAdmin(self,persona): # esta funcion busca una persona y la elimina
-        self.delete_instance(persona)
+    def altaAdmin(self, id,nom,ape,tel,dni,email,usuar): # le voy a pasar por parametros nuevos "atributos", correspondiente a cada atributo de la clae padre en este caso, ejemplo claro de como hereda lso atributos de persona el administrador
+        return Administrador.create(iden=id,nombre=nom,apellido=ape,telefono=tel, documento=dni,mail=email, usuarios=usuar)
+                
+    def eliminarAdmin(self): # elimina lo que esta entre parentesis, en este caso self
+        self.delete_instance()
--- Back/Cliente.py
+++ Back/Cliente.py
@@ -7,3 +7,20 @@ class Cliente(Persona):
         return self.direccion
     def setdireccion(self, new):
         self.direccion = new
+
+    def gethistorial(self):
+        return self.carrito
+    
+    def altaCliente(self,id,nom,ape,tel,dni,email, usuar,dire):
+        return Cliente.create(iden=id,nombre=nom,apellido=ape,telefono=tel, documento=dni,mail=email, usuarios= usuar, direccion= dire)
+    # def inicioSesion() es un bardo
+
+    # def cerrarSesion() es otro bardo
+
+    def verDatosCuenta(self):
+        return [self.iden, self.nombre, self.apellido, self.telefono, self.documento, self.mail, self.usuarios, self.direccion]
+
+    def modificarDatosCiente(self,id):
+        if self.iden != id:
+            self.setiden(id)
+            
